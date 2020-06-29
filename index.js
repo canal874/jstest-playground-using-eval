0f28d972b4436107367ebd78968f7aff7b19fa92
Hidekazu Kubota <hidekazu.kubota@gmail.com>
2020-06-29 14:30:25 +0900

    add say() and completed()

--- index.js
+++ index.js
@@ -4,5 +4,16 @@
 
 function run() {
   const code = document.getElementById("code").value;
+  let completed = function() {
+   document
+      .getElementById("previewArea")
+      .insertAdjacentHTML("beforeend", "Completed!<br>");
+  }
   eval(code);
+  completed();
 }
+
+function say(txt) {
+  document
+    .getElementById("previewArea")
+    .insertAdjacentHTML("beforeend", `${txt}<br>`);
